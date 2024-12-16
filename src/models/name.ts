e920d2fe37e0ad38e881eb803a17262c8044cb9e
nallasuhas <suhasnalla43@gmail.com>
2024-12-16 17:06:39 +0530

    initial commit: data modelling finished

---
+++ src/models/name.ts
@@ -0,0 +1,6 @@
+export const db = "main-codeExchange"
+export const questionCollection = "questions"
+export const answerCollection = "answers"
+export const commentCollection = "comments"
+export const voteCollection = "votes"
+export const attachementBucket = "attachements"
