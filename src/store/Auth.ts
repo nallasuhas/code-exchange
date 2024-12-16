696f7d5539694233e9f9388ca356648a82e44eb9
nallasuhas <suhasnalla43@gmail.com>
2024-12-16 19:48:14 +0530

    auth store setup

--- src/store/Auth.ts
+++ src/store/Auth.ts
@@ -1,24 +1,123 @@
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
-import { persist } from "zustand/middleware"; 
-import {AppwriteException, ID, Models} from "appwrite" 
+import { persist } from "zustand/middleware";
+import { AppwriteException, ID, Models } from "appwrite";
 import { account } from "@/models/client/config";
 
-// interface definitions (for typescript compatibility)
-// every user should have a reputation based on no of upvotes and downvotes
 export interface UserPrefs {
-    reputation: number
-  }
-  interface IAuthStore {
-    session: Models.Session | null; 
-    jwt: string | null
-    user: Models.User<UserPrefs> | null
-    hydrated: boolean
-    // method declarations
-    setHydrated(): void;
-    verfiySession(): Promise<void>;
-    login(email: string, password: string): Promise<{success: boolean; error?: AppwriteException| null}>
-    createAccount(name: string, email: string, password: string): Promise<{success: boolean;  error?: AppwriteException| null}>
-    logout(): Promise<void>
+  reputation: number;
+}
 
-  }
+interface IAuthStore {
+  session: Models.Session | null;
+  jwt: string | null;
+  user: Models.User<UserPrefs> | null;
+  hydrated: boolean;
+  setHydrated: () => void;
+  verfiySession: () => Promise<void>;
+  login: (
+    email: string,
+    password: string
+  ) => Promise<{ success: boolean; error?: AppwriteException | null }>;
+  createAccount: (
+    name: string,
+    email: string,
+    password: string
+  ) => Promise<{ success: boolean; error?: AppwriteException | null }>;
+  logout: () => Promise<void>;
+}
+
+export const useAuthStore = create<IAuthStore>()(
+  persist(
+    immer<IAuthStore>((set) => ({
+      session: null,
+      jwt: null,
+      user: null,
+      hydrated: false,
+
+      setHydrated() {
+        set((state) => {
+          state.hydrated = true;
+        });
+      },
+
+      async verfiySession() {
+        try {
+          const session = await account.getSession("current");
+          set((state) => {
+            state.session = session;
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      },
+
+      async login(email, password) {
+        try {
+          const session = await account.createEmailPasswordSession(
+            email,
+            password
+          );
+          const [user, { jwt }] = await Promise.all([
+            account.get<UserPrefs>(),
+            account.createJWT(),
+          ]);
+
+          if (!user.prefs?.reputation) {
+            await account.updatePrefs<UserPrefs>({
+              reputation: 0,
+            });
+          }
+
+          set((state) => {
+            state.session = session;
+            state.user = user;
+            state.jwt = jwt;
+          });
+
+          return { success: true };
+        } catch (error) {
+          console.log(error);
+          return {
+            success: false,
+            error: error instanceof AppwriteException ? error : null,
+          };
+        }
+      },
+
+      async createAccount(name, email, password) {
+        try {
+          await account.create(ID.unique(), email, password, name);
+          return { success: true };
+        } catch (error) {
+          console.log(error);
+          return {
+            success: false,
+            error: error instanceof AppwriteException ? error : null,
+          };
+        }
+      },
+
+      async logout() {
+        try {
+          await account.deleteSessions();
+          set((state) => {
+            state.session = null;
+            state.jwt = null;
+            state.user = null;
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    })),
+    {
+      name: "auth",
+      onRehydrateStorage() {
+        return (state, error) => {
+          if (!error) state?.setHydrated();
+        };
+      },
+    }
+  )
+);
